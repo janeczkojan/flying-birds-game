9b50a7e18546f5b33b98667505df29c1283f0e11
janeczko.jan <janeczko.jan@hotmail.com>
2024-12-01 18:19:17 +0100

    Prepare some components

---
+++ src/components/bird/bird.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bird',
+  imports: [],
+  templateUrl: './bird.component.html',
+  styleUrl: './bird.component.scss'
+})
+export class BirdComponent {}
---
+++ src/components/flying-area/flying-area.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-flying-area',
+  imports: [],
+  templateUrl: './flying-area.component.html',
+  styleUrl: './flying-area.component.scss'
+})
+export class FlyingAreaComponent {}
