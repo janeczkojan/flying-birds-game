958e2c74c51e81b19c743318a2c0a4d63a01b3d9
janeczko.jan <janeczko.jan@hotmail.com>
2024-12-02 22:02:11 +0100

    Prepare - bird animation to follow mouse

--- src/views/game/game.component.ts
+++ src/views/game/game.component.ts
@@ -1,4 +1,11 @@
-import { ChangeDetectionStrategy, Component, computed } from '@angular/core';
+import {
+  AfterViewInit,
+  ChangeDetectionStrategy,
+  Component,
+  computed,
+  DestroyRef,
+  signal
+} from '@angular/core';
 import { FlyingAreaComponent } from '../../components/flying-area/flying-area.component';
 import { BirdComponent } from '../../components/bird/bird.component';
 import { Position, Size } from '../../types';
@@ -9,15 +16,22 @@ import {
   defaultIfEmpty,
   filter,
   fromEvent,
+  interval,
   map,
+  merge,
   of,
-  startWith
+  startWith,
+  take,
+  tap
 } from 'rxjs';
-import { toSignal } from '@angular/core/rxjs-interop';
+import { takeUntilDestroyed, toSignal } from '@angular/core/rxjs-interop';
+import { radiansToDegrees } from '../../utils/math';
 
 const RESIZE_DEBOUNCE_MS = 10;
 const MOUSE_MOVE_DEBOUNCE_MS = 4;
+const ANIMATION_INTERVAL_MS = 20;
 const BIRD_WIDTH_DIVIDER = 10;
+const MOVEMENT_SIZE = 1;
 
 @Component({
   selector: 'app-game',
@@ -26,22 +40,25 @@ const BIRD_WIDTH_DIVIDER = 10;
   styleUrl: './game.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class GameComponent {
-  private windowSize$ = combineLatest([
-    of(null),
-    fromEvent(window, 'resize').pipe(debounceTime(RESIZE_DEBOUNCE_MS))
-  ]).pipe(
-    startWith(this.getCurrentWindowSize()),
-    map(() => this.getCurrentWindowSize())
+export class GameComponent implements AfterViewInit {
+  private readonly windowSize$ = merge(
+    of(this.getCurrentWindowSize()),
+    fromEvent(window, 'resize').pipe(
+      debounceTime(RESIZE_DEBOUNCE_MS),
+      map(() => this.getCurrentWindowSize())
+    )
   );
 
-  private mousePosition$ = fromEvent(document, 'mousemove').pipe(
-    debounceTime(MOUSE_MOVE_DEBOUNCE_MS),
-    map((event) => event as MouseEvent),
-    map((event): Position => [event.clientX, event.clientY]),
-    catchError(() => of(null)),
-    filter((value) => !!value),
-    defaultIfEmpty(this.getTopRightCorner())
+  private readonly mousePosition$ = merge(
+    of(this.getTopRightCorner()),
+    fromEvent(document, 'mousemove').pipe(
+      debounceTime(MOUSE_MOVE_DEBOUNCE_MS),
+      map((event) => event as MouseEvent),
+      map((event): Position => [event.clientX, event.clientY]),
+      catchError(() => of(null)),
+      filter((value) => !!value),
+      defaultIfEmpty(this.getTopRightCorner())
+    )
   );
 
   protected readonly windowSize = toSignal(this.windowSize$, {
@@ -52,13 +69,21 @@ export class GameComponent {
     initialValue: this.getTopRightCorner()
   });
 
-  // TODO calculate proper position - not computed, change to WritableSignal
-  // Change position over time based on angle of mouse related to current position
-  protected readonly birdPosition = computed(() => this.mousePosition());
+  protected readonly birdPosition = signal(this.getInitBirdPosition());
 
   protected readonly birdWidth = computed(
     () => this.windowSize()[0] / BIRD_WIDTH_DIVIDER
   );
+
+  // TODO remove
+  protected readonly angle = signal(0);
+  protected readonly sector = signal(1);
+
+  constructor(private readonly destroyRef: DestroyRef) {}
+
+  ngAfterViewInit(): void {
+    this.startAnimationLoop();
+  }
 
   private getCurrentWindowSize(): Size {
     return [window.innerWidth, window.innerHeight];
@@ -66,5 +91,56 @@ export class GameComponent {
 
   private getTopRightCorner(): Position {
     return [this.getCurrentWindowSize()[0], 0];
+  }
+
+  private getInitBirdPosition(): Position {
+    const [x, y] = this.getCurrentWindowSize();
+    return [x / 2 - 50, y / 2 - 50];
+  }
+
+  private animationLoopStep(): Promise<void> {
+    const [mouseX, mouseY] = this.mousePosition();
+    const [birdX, birdY] = this.birdPosition();
+
+    return new Promise((resolve) => {
+      try {
+        const xDistance = Math.abs(mouseX - birdX);
+        const yDistance = Math.abs(mouseY - birdY);
+        const distance = Math.sqrt(
+          xDistance * xDistance + yDistance * yDistance
+        );
+
+        let alphaAngle = radiansToDegrees(Math.asin(yDistance / distance));
+        let sector = 1; // TODO needed?
+
+        // TODO calculate next position based on angle
+        if (mouseX <= birdX && mouseY <= birdY) {
+          sector = 2;
+          alphaAngle = 180 - alphaAngle;
+        } else if (mouseX <= birdX && mouseY >= birdY) {
+          sector = 3;
+          alphaAngle = 180 + alphaAngle;
+        } else if (mouseX >= birdX && mouseY >= birdY) {
+          sector = 4;
+          alphaAngle = 360 - alphaAngle;
+        }
+
+        this.angle.set(alphaAngle);
+        this.sector.set(sector);
+      } catch (err) {
+        console.error(err);
+      }
+
+      resolve();
+    });
+  }
+
+  private startAnimationLoop(): void {
+    interval(ANIMATION_INTERVAL_MS)
+      .pipe(
+        tap(() => this.animationLoopStep()),
+        takeUntilDestroyed(this.destroyRef)
+      )
+      .subscribe();
   }
 }
