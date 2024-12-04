7fa6dfff10fa3061a56c606791f7f14ea56dbab5
janeczko.jan <janeczko.jan@hotmail.com>
2024-12-04 20:45:07 +0100

    Add button component

---
+++ src/components/button/button.component.ts
@@ -0,0 +1,11 @@
+import { Component, output } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  imports: [],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.scss'
+})
+export class ButtonComponent {
+  protected readonly click = output<MouseEvent>();
+}
--- src/views/home-view/home-view.component.ts
+++ src/views/home-view/home-view.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { GameStateService } from '../../services/game-state.service';
 import { Router } from '@angular/router';
+import { ButtonComponent } from '../../components/button/button.component';
 
 @Component({
   selector: 'app-home-view',
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './home-view.component.html',
   styleUrl: './home-view.component.scss'
 })
